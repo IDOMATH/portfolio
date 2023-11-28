43da7835e34afa6161d5b6e02f25ba1208f703b4
IDOMATH <btdothemath@gmail.com>
2023-11-27 21:20:17 -0600

    Starting setup for signup handler

---
+++ handlers/signup_handler.go
@@ -0,0 +1,13 @@
+package handlers
+
+import "github.com/IDOMATH/portfolio/db"
+
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
