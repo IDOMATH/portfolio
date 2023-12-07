1749c7a33cfbc1d4219e77a139f4ea7a9adc98ec
IDOMATH <btdothemath@gmail.com>
2023-12-07 15:08:30 -0600

    Adding query to all guestbook signatures handler

--- handlers/guestbook_handler.go
+++ handlers/guestbook_handler.go
@@ -45,6 +45,17 @@ func (h *GuestbookHandler) HandlePostGuestbookSignature() http.HandlerFunc {
 }
 
 func (h *GuestbookHandler) HandleGetAllGuestbookSignature() http.HandlerFunc {
+
+	signatures, err := h.guestbookStore.GetAllGuestbookSignatures()
+	objects := make(map[string]interface{})
+	objects["signatures"] = signatures
+
+	if err != nil {
+		return func(w http.ResponseWriter, r *http.Request) {
+			util.WriteError(w, http.StatusInternalServerError, err)
+		}
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 
 	}
