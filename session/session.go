4a825ca6a27a7726e3636305c99bf470e5be4b0d
IDOMATH <btdothemath@gmail.com>
2024-01-24 20:24:45 -0600

    Starting on making session

--- session/session.go
+++ session/session.go
@@ -1,1 +1,40 @@
 package session
+
+import (
+	"sync"
+	"time"
+)
+
+// TODO: Decide if I want to use a session
+type Manager struct {
+	name        string
+	lock        sync.Mutex
+	sessions    map[string]interface{}
+	key         sessionKey
+	maxLifetime time.Duration
+}
+
+type sessionKey struct{}
+
+func NewManager(name string, lifetime time.Duration) *Manager {
+	return &Manager{
+		name:        name,
+		sessions:    make(map[string]interface{}),
+		key:         sessionKey{},
+		maxLifetime: lifetime,
+	}
+}
+
+type Provider interface {
+	SessionInit(sid string) (Session, error)
+	SessionRead(sid string) (Session, error)
+	SessionDestroy(sid string) error
+	SessionGC(maxLifetime int64)
+}
+
+type Session interface {
+	Set(key, value interface{}) error
+	Get(key interface{}) interface{}
+	Delete(key interface{}) error
+	SessionId() string
+}
