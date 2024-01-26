c4f2e0d72be74d1c1a83f7ef217daee4ecfc5462
IDOMATH <btdothemath@gmail.com>
2024-01-26 13:31:10 -0600

    Session test

---
+++ session/session_test.go
@@ -0,0 +1,37 @@
+package session
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+	"time"
+)
+
+type Session struct {
+	name string
+}
+
+func FakeLogin(sess *Manager) {
+	req, _ := http.NewRequest("GET", "/test", nil)
+	rr := httptest.NewRecorder()
+	sess.PutSession(rr, req, &Session{name: "testing"})
+}
+
+func TestSessionManager(t *testing.T) {
+	sess := NewManager("test", time.Minute*5)
+
+	var wg sync.WaitGroup
+	numGoroutines := 20
+	for i := 0; i <= numGoroutines; i++ {
+		go func() {
+			defer wg.Done()
+			for created := 0; created <= 50000; created++ {
+				FakeLogin(sess)
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Println("10 million sessions simulated")
+}
