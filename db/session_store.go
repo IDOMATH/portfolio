dc20ae8d6a7125ea76266e841e5258244ef7b648
IDOMATH <btdothemath@gmail.com>
2024-02-12 20:49:13 -0600

    Adding method to check session token in DB

--- db/session_store.go
+++ db/session_store.go
@@ -33,3 +33,24 @@ func (s *PostgresSessionStore) InsertSessionToken(token string) (int, error) {
 	}
 	return newId, nil
 }
+
+func (s *PostgresSessionStore) CheckSessionToken(token string) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var sessionToken string
+	var expiresAt time.Time
+	statement := `SELECT token, expires_at FROM auth_tokens 
+				  WHERE token = $1 `
+
+	err := s.DB.QueryRowContext(ctx, statement,
+		token,
+		time.Now().Add(time.Hour*2)).Scan(sessionToken, expiresAt)
+	if err == sql.ErrNoRows {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
